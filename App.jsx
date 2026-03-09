e1655b67ac8b49d755dfb75e8a0b39bacca9f836
Mr. Rutik Patil <patilrutik500@gmail.com>
2026-03-10 00:46:42 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import Data from './data';
+import List from './list';
+
+function App() {
+  const [people, setPeople] = useState(Data);
+
+  return (
+    <div className='container'>
+      <h3>{people.length} Students</h3>
+      <List people={people}/>
+      <button onClick={() => setPeople([])}>Clear All</button>
+    </div>
+  );
+}
+
+export default App;
